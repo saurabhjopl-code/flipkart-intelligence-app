4ce86db2f1c00bc1ed60bae576c94e2f4d29a4a0
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-03-12 11:44:19 +0530

    Update dataStore.js

--- js/core/dataStore.js
+++ js/core/dataStore.js
@@ -1,1 +1,21 @@
-// Placeholder file for dataStore.js
+// js/core/dataStore.js
+
+export const dataStore = {
+
+    CDR: [],
+    CFR: [],
+    PPR: [],
+    CKR: [],
+    GMV: [],
+    FCSTOCK: [],
+    RATINGS: []
+
+};
+
+export function setData(sheetName, data) {
+    dataStore[sheetName] = data;
+}
+
+export function getData(sheetName) {
+    return dataStore[sheetName] || [];
+}
